a60f96138b26ef5b4cd2e798849ce1535c63e4bd
Alexandr Zhelanov <AlexandrZhelanov@gmail.com>
2026-07-03 19:29:57 +0300

    Tutorial example

---
+++ UsefulPractices/UsefulPractices/UsefulPractices.docc/step_1.swift
@@ -0,0 +1,17 @@
+//
+//  step_1.swift
+//  
+//
+//  Created by Aleksandr Zhelanov on 03.07.2026.
+//
+
+import UIKit
+
+struct StepStructure {
+    private let someVariable: Int = 0
+    
+    func increment() -> Int {
+        someVariable += 1
+        return someVariable
+    }
+}
